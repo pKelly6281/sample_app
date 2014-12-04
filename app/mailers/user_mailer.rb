23bbee0fe4bdfde53626339de1db101ae49d48a4
Patrick Kelly <pk6281@mcla.edu>
2014-12-04 10:45:10 -0500

    Finished up to 10.48

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,7 +9,6 @@ class UserMailer < ActionMailer::Base
   def account_activation(user)
     @user = user
     mail to: user.email, subject: "Account activation"
-
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -18,8 +17,7 @@ class UserMailer < ActionMailer::Base
   #   en.user_mailer.password_reset.subject
   #
   def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
 end
